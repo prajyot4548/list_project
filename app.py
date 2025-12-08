a7de0427dc6de2384f478dcf9ad20133747005e7
Prajyot Hupare <your_github_email_here>
2025-12-08 12:29:40 +0530

    Updated HTML and Python changes

--- app.py
+++ app.py
@@ -26,6 +26,7 @@ def get_db():
         host=os.environ.get("DB_HOST"),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
+        
         database=os.environ.get("DB_NAME"),
         port=int(os.environ.get("DB_PORT"))
     )
